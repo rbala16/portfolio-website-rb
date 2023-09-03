aab940a49139c7ffa96400771c0a9aa1e20a2646
Rajni bala <balatamna16@gmail.com>
2023-09-03 15:02:38 -0400

    import all components in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import React from 'react';
+import Header from './components/header';
+import Navigation from './components/navigation';
+import About from './components/about';
+import Experience from './components/Experience';
+import Project from './components/project';
+import Contact from './components/contact';
+import Footer from './components/footer';
+import Testimonial from './components/testimonials';
 const App = () => {
   return (
-    <div>App</div>
+   <>
+   <Header/>
+   <Navigation/>
+   <About/>
+   <Experience/>
+   <Project/>
+   <Testimonial/>
+   <Contact/>
+   <Footer/>
+
+   </>
   )
 }
 
