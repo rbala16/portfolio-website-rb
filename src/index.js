64c0d8c9b0643b8049a61d01ec63956e615fe0b1
Rajni bala <balatamna16@gmail.com>
2023-09-03 14:46:07 -0400

    assign components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-
+// import React from "react";
+import App  from "./App";
 import ReactDOM from 'react-dom';
-
+import "./index.css";
 ReactDOM.render(
 <App/>,
  document.getElementById('#root')
